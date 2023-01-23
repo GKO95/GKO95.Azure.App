b2a29c475b44f2bd3906ab0ff4b77006da7935c5
Gihwan Ko <gihwanko@microsoft.com>
2023-01-23 21:48:43 +0900

    Rename and furnish GitHubService class

---
+++ GKO95.Azure.App/Data/GitHubService.cs
@@ -0,0 +1,17 @@
+﻿using Octokit;
+
+namespace GKO95.Azure.App.Data
+{
+	public class GitHubService
+	{
+		public readonly GitHubClient github = new(new ProductHeaderValue("GKO95.Azure.app"));
+		
+		public GitHubService()
+		{
+			
+		}
+
+		public Task<User> GetGitHubUserAsync(string username) => github.User.Get(username);
+		public Task<Repository> GetGitHubRepositoryAsync(string username, string repository) => github.Repository.Get(username, repository);
+	}
+}
